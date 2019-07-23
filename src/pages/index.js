21ec019f95ab2a71b599e8a10f76c44012f2bd92
rishishah <rishishah.multidots@gmail.com>
2019-07-23 15:42:20 +0530

    Added

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ class Index extends Component {
   }
 
   componentDidMount() {
-    return fetch(`http://wptest.io/wp-json/wp/v2/pages`)
+    return fetch(`https://wptest.io/wp-json/wp/v2/pages`)
     .then((response) => response.json())
     .then((responseJson) => {
       this.setState({
@@ -28,40 +28,38 @@ class Index extends Component {
   render() {
     return (
       <div className="main-container">
-<section id="banner">
-  <div className="content">
-	<header>
-	  <h1>Hi, I’m <br />
-	  Rishi Shah</h1>
-	  <p>A Web Developer</p>
-	</header>
-	<p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. Pellentesque sapien ac quam. Lorem ipsum dolor sit nullam.</p>
-	<ul class="actions"><li><Link to="/about" className="button primary">About Me</Link></li></ul>
-  </div>
-  <span className="image object">
-	<img src={rishi_shah} alt="" />
-  </span>
-</section>
-<hr />
+        <section id="banner">
+          <div className="content">
+          <header>
+            <h1>Hi, I’m <br />
+            Rishi Shah</h1>
+            <p>A Web Developer</p>
+          </header>
+          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. Pellentesque sapien ac quam. Lorem ipsum dolor sit nullam.</p>
+          <ul class="actions"><li><Link to="/about" className="button primary">About Me</Link></li></ul>
+          </div>
+          <span className="image object">
+          <img src={rishi_shah} alt="" />
+          </span>
+        </section>
+        <hr />
 
-<section>
-  <header className="major">
-	<h2>Blogs</h2>
-  </header>
-  <div className="posts">
-
-          {this.state.data.map((item, index) => (
-            
-            <article>
-              {/* {console.log(item.excerpt.rendered)} */}
-            <a href="#" className="image"><img src={banner} alt="" /></a>
-            <h3>{item.title.rendered}</h3>
-            <div className= "content" dangerouslySetInnerHTML={{ __html: item.excerpt.rendered }} />
-            <ul class="actions"><li><Link to="/single-blog" className="button">Read More</Link></li></ul>
-          </article>
+        <section>
+          <header className="major">
+          <h2>Blogs</h2>
+          </header>
+          <div className="posts">
+            {this.state.data.map((item, index) => (
+              <article>
+                  {/* {console.log(item.excerpt.rendered)} */}
+                <a href="#" className="image"><img src={banner} alt="" /></a>
+                <h3>{item.title.rendered}</h3>
+                <div className= "content" dangerouslySetInnerHTML={{ __html: item.excerpt.rendered }} />
+                <ul class="actions"><li><Link to="/single-blog" className="button">Read More</Link></li></ul>
+              </article>
             ))}
-       </div>
-</section>
+              </div>
+        </section>
       </div>
     )
   }
